8788b8c22ba0a741b8e1650187125d2ca765901e
45sjp <45sjp@naver.com>
2022-06-03 22:21:26 +0900

    220603 communityBoard.jsp 레이아웃 작성

---
+++ src/main/java/board/controller/community/CommunityBoardServlet.java
@@ -0,0 +1,25 @@
+package board.controller.community;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class CommunityBoardServlet
+ */
+@WebServlet("/board/community")
+public class CommunityBoardServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/views/board/community/communityBoard.jsp")
+			.forward(request, response);
+	}
+
+}
