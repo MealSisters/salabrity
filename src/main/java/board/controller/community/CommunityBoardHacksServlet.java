b7e9da60a6dcfb6cbd843702eb866d4b0561e358
45sjp <45sjp@naver.com>
2022-06-04 21:44:34 +0900

    220604 communityBoard.jsp css 작성 및 communityBoardList.jsp 레이아웃 초안

---
+++ src/main/java/board/controller/community/CommunityBoardHacksServlet.java
@@ -0,0 +1,25 @@
+package board.controller.community;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class CommunityBoardHacksServlet
+ */
+@WebServlet("/board/community/general")
+public class CommunityBoardHacksServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/views/board/community/communityBoardList.jsp")
+			.forward(request, response);
+	}
+
+}
