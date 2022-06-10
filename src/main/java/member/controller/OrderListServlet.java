0206894cd48d2bdc482238c401477e5ffe15e23b
tichita <maingo09@hanmeil.net>
2022-06-10 09:29:15 +0900

    (220610) orderList 추가(1차)

---
+++ src/main/java/member/controller/OrderListServlet.java
@@ -0,0 +1,25 @@
+package member.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class OrderList
+ */
+@WebServlet("/mypage/orderList")
+public class OrderListServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.getRequestDispatcher("/WEB-INF/views/member/mypage/orderList.jsp").forward(request, response);
+	}
+
+}
