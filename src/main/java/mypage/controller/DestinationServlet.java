bbae41b66f09a2226e16f547b4552f90a4067309
tichita <maingo09@hanmeil.net>
2022-06-11 21:25:51 +0900

    (220611) destination 화면구현

---
+++ src/main/java/mypage/controller/DestinationServlet.java
@@ -0,0 +1,24 @@
+package mypage.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class DestinationServlet
+ */
+@WebServlet("/mypage/destination")
+public class DestinationServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/views/member/mypage/destination.jsp").forward(request, response);
+	}
+
+}
