fe6f4e2e6dbfd54dd57206fc1c37a7957d99737a
venandi48 <bow999@naver.com>
2022-06-13 23:47:34 +0900

    220613 메뉴등록 유효성검사

---
+++ src/main/java/admin/controller/MenuIdCheckServlet.java
@@ -0,0 +1,45 @@
+package admin.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+import admin.model.service.AdminService;
+import menu.model.dto.Menu;
+
+/**
+ * Servlet implementation class MenuNameCheckServlet
+ */
+@WebServlet("/admin/menuIdCheck")
+public class MenuIdCheckServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private AdminService adminService = new AdminService();
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			String inputId = request.getParameter("menuId");
+
+			int idCheck = 0;
+			List<Menu> list = adminService.findMenuById(inputId);
+			if (!list.isEmpty())
+				idCheck = list.size();
+
+			response.setContentType("application/json; charset=utf-8");
+			new Gson().toJson(idCheck, response.getWriter());
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+	}
+
+}
