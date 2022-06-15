bf345447c408af0398c52028c17e855cfb9103bf
venandi48 <bow999@naver.com>
2022-06-15 15:22:09 +0900

    220615 상품등록서블릿
    미완성

--- src/main/java/admin/controller/AdminProductEnrollServlet.java
+++ src/main/java/admin/controller/AdminProductEnrollServlet.java
@@ -9,6 +9,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.oreilly.servlet.MultipartRequest;
+import com.oreilly.servlet.multipart.FileRenamePolicy;
+
+import common.SalabrityFileRenamePolicy;
 import menu.model.dto.Menu;
 import menu.model.service.MenuService;
 
@@ -35,8 +39,17 @@ public class AdminProductEnrollServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		try {
+			String saveDirectory = getServletContext().getRealPath("/images/product");
+			int maxPostSize = 1024 * 1024 * 10; // 메뉴 썸네일은 최대 10MB
+			String encoding = "utf-8";
+			FileRenamePolicy policy = new SalabrityFileRenamePolicy();
+			MultipartRequest multiReq = 
+					new MultipartRequest(request, saveDirectory, maxPostSize, encoding, policy);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
