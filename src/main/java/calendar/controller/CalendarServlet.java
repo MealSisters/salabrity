d4b56b64b87c16306ebafb062270a0c4406ee9df
venandi48 <bow999@naver.com>
2022-06-16 21:11:41 +0900

    220616 데브 커밋

--- src/main/java/calendar/controller/CalendarServlet.java
+++ src/main/java/calendar/controller/CalendarServlet.java
@@ -1,11 +1,20 @@
 package calendar.controller;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import calendar.model.dto.CalendarExt;
+import calendar.model.service.CalendarService;
+import product.model.dto.ProductExt;
+import product.model.service.ProductService;
 
 /**
  * Servlet implementation class CalendarServlet
@@ -13,12 +22,40 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/calendar")
 public class CalendarServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private CalendarService calendarService = new CalendarService();
+	private ProductService productService = new ProductService();
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getRequestDispatcher("/WEB-INF/views/calendar/calendar.jsp").forward(request, response);
+		try {
+			List<CalendarExt> calList = calendarService.findRecentProductCal();
+
+			List<ProductExt> productAll = productService.findProductsAll();
+			ProductExt recentProduct = null;
+			if(productAll != null && !productAll.isEmpty())
+				recentProduct = productAll.get(0);
+			
+			ProductExt productHere = null;
+			Map<String, CalendarExt> calMap = new HashMap<>();
+			if (calList != null && !calList.isEmpty()) {
+				productHere = productService.findProductByNo(calList.get(0).getProductNo());
+				for(CalendarExt cal : calList) {
+					calMap.put(cal.getWeekDayCode().toString(), cal);
+				}
+			}
+			System.out.println("calMap = " + calMap);
+
+			request.setAttribute("productHere", productHere);
+			request.setAttribute("recentProduct", recentProduct);
+			request.setAttribute("productList", productAll);			
+			request.setAttribute("calMap", calMap);
+			request.getRequestDispatcher("/WEB-INF/views/calendar/calendar.jsp").forward(request, response);
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
 	}
 
 	/**
