4743f1c91deb8bb03a1366265afb4074d7d16bbe
45sjp <45sjp@naver.com>
2022-06-20 21:45:41 +0900

    220620 [feature/main] 메인화면 기능구현 완료

---
+++ src/main/java/common/filter/BoardLoginFilter.java
@@ -0,0 +1,73 @@
+package common.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import member.model.dto.Member;
+
+/**
+ * 
+ */
+@WebFilter({
+	"/board/community/hacksEnroll",
+	"/board/community/hacksUpdate",
+	"/board/community/hacksDelete",
+	"/board/community/hacksView/*",
+	"/board/community/hacksFileDownload",
+	"/board/community/generalEnroll",
+	"/board/community/generalUpdate",
+	"/board/community/generalDelete",
+	"/board/community/generalView/*",
+	"/board/community/generalFileDownload"
+})
+public class BoardLoginFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public BoardLoginFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest httpReq = (HttpServletRequest) request;
+		HttpServletResponse httpRes = (HttpServletResponse) response;
+		
+		HttpSession session = httpReq.getSession();
+		Member loginMember = (Member) session.getAttribute("loginMember");
+		
+		if(loginMember == null) {
+			session.setAttribute("msg", "로그인 후 이용하실 수 있습니다.");
+			httpRes.sendRedirect(httpReq.getContextPath() + "/");
+			return;
+		}
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
